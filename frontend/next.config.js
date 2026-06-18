13cef533a003405654bc72c97ebd4df2e37c3ec3
touzgar <ghaithslama115@gmail.com>
2026-06-19 00:27:33 +0100

    Ready for Vercel deployment - Full Stack Next.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
 
 const nextConfig = {
   images: {
@@ -160,4 +157,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withBundleAnalyzer(nextConfig);
+module.exports = nextConfig;
